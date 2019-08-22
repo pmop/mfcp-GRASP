554a0b1189676db6f44d2418a2bfa5adc33b38c3
Pedro Matheus <pedro.matheus123@gmail.com>
2019-08-22 12:51:25 -0300

    Few implementations.

--- main.py
+++ main.py
@@ -7,13 +7,48 @@ def cartesian_distance_point_to_point(x, y, sx, sy):
 
 # TODO
 def grasp_build_greedy_rand_solution(a, random_seed, p, js):
-    pass
+    js = []
+    for i in range(p):
+        # RCL = make_rcl(a,j,js,gamma)
+        # s = select_facility(rcl, random_seed, js)
+        # js.append(s) # js = js union {s}
+        # adapt_greedy_function(s, j, js, gM, gM-1, gamma)
+        pass
+
+
+def max_gamma(ys):
+    m = 0.0
+    for gs in ys:
+        if gs > m:
+            m = gs
+
+
+def yet_to_be_selected(locations_selected, candidate_locations):
+    unselected_locations = []
+    for location in candidate_locations:
+        if location not in locations_selected:
+            unselected_locations.append(location)
+    return unselected_locations
+
+
+# TODO restriction mechanism function ( restricted candidate list )
+def make_rcl(a, j, js, gamma):
+    rcl = []
+    yjjs = yet_to_be_selected(j,js)
+    gamma_st = max_gamma(sum_weights(yjjs))
+    for s in yjjs:
+        if sum_weights(s) >= a * gamma_st:
+            rcl.append(s)
+    return rcl
 
 
 # TODO
 def local_search(js):
     pass
 
+# TODO
+def sum_weight_location(location):
+    pass
 
 # TODO
 def sum_weights(js):
@@ -24,7 +59,7 @@ def sum_weights(js):
 def grasp(a, max_iter, random_seed):
     best_solution_found = None
     p = None
-    js = None
+    js = []
     for _ in range(max_iter):
         grasp_build_greedy_rand_solution(a, random_seed, p, js)
         local_search(js)
