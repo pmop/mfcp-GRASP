4ca173d8d25a078c43d3039827bea56033dc7ec9
Pedro Matheus <pedro.matheus123@hotmail.com>
2019-09-04 20:12:46 -0300

    Multithreaded implementation for interactivity.

--- grasp-modified-solution.py
+++ grasp-modified-solution.py
@@ -1,6 +1,15 @@
 from random import randint
 from math import sqrt
+from copy import deepcopy
 import pygame
+import threading
+import time
+
+mlock = threading.Condition()
+GSOL = []
+GBOUNDS = []
+GPARTIAL = []
+GRUN = True
 
 
 def cartesian_distance_point_to_point(A, B):
@@ -9,11 +18,11 @@ def cartesian_distance_point_to_point(A, B):
     return sqrt((sx - x) ** 2 + (sy - y) ** 2)
 
 
-def generate_random_points(width, height, p):
+def generate_random_points(lower, upper, p):
     points = []
     for _ in range(p):
-        x = randint(0, width)
-        y = randint(0, height)
+        x = randint(lower[0], upper[0])
+        y = randint(lower[1], upper[1])
         points.append([x, y])
     points.sort(key=lambda k: k[0] + k[1])
     return points
@@ -47,16 +56,45 @@ def heightmost_demand(demand_points):
     return heightmost
 
 
-def greedy_random_solution(demand_points, radius):
+def get_bounds(demand_points):
+    minX = demand_points[0][0]
+    maxX = 0
+    minY = demand_points[0][1]
+    maxY = 0
+    for point in demand_points:
+        x, y = point[0], point[1]
+        if x > maxX:
+            maxX = x
+        if y > maxY:
+            maxY = y
+        if x < minX:
+            minX = x
+        if y < minY:
+            minY = y
+    lower = [minX, minY]
+    upper = [maxX, maxY]
+    return lower, upper
+
+
+def vec_scalar_sum(vec, s):
+    for i in range(len(vec)):
+        vec[i] += s
+    return vec
+
+
+def greedy_random_solution(demand_points, radius, p):
+    global GRUN
+    global GSOL
+    global GBOUNDS
+
     sol = []
     uncovered_demand = [i for i in demand_points]
-    while len(uncovered_demand) > 0:
+    while GRUN and len(uncovered_demand) > 0:
         sol_candidates = []
-        leftmost_demand = uncovered_demand[0][0] + radius
-        heightmost = heightmost_demand(uncovered_demand) + radius
+        lower, upper = get_bounds(uncovered_demand)
+
         step_solution_covered_points = []
-        p = len(uncovered_demand)  # generate at most p facilities
-        sol_ = generate_random_points(leftmost_demand, heightmost, p)
+        sol_ = generate_random_points(lower, upper, p)
         for facility in sol_:
             # points_covered is a list of indexes of uncovered_demand that are now covered
             points_covered = points_facility_covers(uncovered_demand, facility, radius)
@@ -72,16 +110,25 @@ def greedy_random_solution(demand_points, radius):
                 uncovered_demand = uncovered
 
         sol = sol + sol_candidates
+        p = len(uncovered_demand)  # generate at most p facilities
+        mlock.acquire()
+        GSOL = deepcopy(sol)
+        GBOUNDS = deepcopy([lower, upper])
+        mlock.release()
+        time.sleep(1)
 
     return sol
 
 
 def grasp(demand_points, radius, max_iter):
-    best = greedy_random_solution(demand_points, radius)
+    global GRUN
+    best = greedy_random_solution(demand_points, radius, len(demand_points))
     best_l = len(best)
     solutions_costs = [best_l]
     for _ in range(max_iter - 1):
-        sol = greedy_random_solution(demand_points, radius)
+        if GRUN is False:
+            break
+        sol = greedy_random_solution(demand_points, radius, best_l)
         j = len(sol)
         solutions_costs.append(j)
         if j < best_l:
@@ -102,11 +149,20 @@ def solution(width, height, demand_points, radius):
     return sol
 
 
-def show_sol(dpoints, floc, fradius):
+def show_sol(dpoints, fradius):
+    global GBOUNDS
+    global GPARTIAL
+    global GSOL
+    global GRUN
+
+    dbounds = None
+    dsol = None
+
     pygame.init()
-    screen = pygame.display.set_mode((640, 480))
+    screen = pygame.display.set_mode((800, 600))
     surface = pygame.Surface(screen.get_size())
     running = True
+    clock = pygame.time.get_ticks()
 
     while running:
         surface.fill((0, 0, 0))
@@ -114,10 +170,29 @@ def show_sol(dpoints, floc, fradius):
         event = pygame.event.wait()
         if event.type == pygame.QUIT:
             running = False
+            GRUN = False
+
         for point in dpoints:
-            pygame.draw.circle(screen, (255, 255, 255), (point[0], point[1]), 2, 0)
-        for facility in floc:
-            pygame.draw.circle(screen, (0, 0, 255), (facility[0], facility[1]), fradius, 1)
+            pygame.draw.circle(screen, (255, 255, 255), (point[0], point[1]), 3)
+
+        if dbounds is not None and (dsol is not None):
+            r = pygame.Rect((dbounds[0][0], dbounds[0][1]), (dbounds[1][0], dbounds[1][1]))
+            pygame.draw.rect(screen, (0, 255, 0), r, 1)
+            for facility in dsol:
+                pygame.draw.circle(screen, (0, 0, 255), (facility[0], facility[1]), fradius, 1)
+
+        # Update our copy of the solution every 500 ms
+        if clock == 0:
+            mlock.acquire()
+            dbounds = deepcopy(GBOUNDS)
+            dsol = deepcopy(GSOL)
+            mlock.release()
+            clock = pygame.time.get_ticks()
+        elif clock > 40:
+            clock = 0
+        else:
+            clock = pygame.time.get_ticks()
+
         pygame.time.wait(500)
         pygame.display.flip()
     pygame.quit()
@@ -148,13 +223,24 @@ def brute_force_improving(demand_points, facilities_points, radius):
 
 
 def simul():
+    global GBOUNDS
+    global GRUN
+
     radius = 30
-    demand_points = generate_random_points(620, 460, 50)
-    print("Waiting for solution.(GRASP-MODIFIED)")
-    sol = grasp(demand_points, radius, 50)
-    w = len(sol)
-    print(f"After improving: {w}")
-    show_sol(demand_points, sol, radius)
+    demand_points = generate_random_points([0, 0], [790, 590], 500)
+    GBOUNDS = [[0, 0], [620, 460]]
+    # print("Waiting for solution.(GRASP-MODIFIED)")
+    # sol = grasp(demand_points, radius, 50)
+    solver_proc = threading.Thread(target=grasp, args=[demand_points, radius, 50])
+    solver_proc.start()
+    show_proc = threading.Thread(target=show_sol, args=[demand_points, radius])
+    show_proc.start()
+    show_proc.join()
+    # show_sol(demand_points, radius)
+    if solver_proc.is_alive():
+        GRUN = False
+    # w = len(sol)
+    # print(f"After improving: {w}")
 
 
 simul()
